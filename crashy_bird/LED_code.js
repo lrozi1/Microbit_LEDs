3685141204bc9041e6b2ae2f0f2fe8fe68d22936
Zayatsoff <47332155+Zayatsoff@users.noreply.github.com>
2019-03-05 13:08:30 -0500

    Update and rename Gal to LED_code.js

---
+++ crashy_bird/LED_code.js
@@ -0,0 +1,61 @@
+let ticks = 0
+let emptyObstacleY = 0
+let bird: game.LedSprite = null
+let obstacles: game.LedSprite[] = []
+let index = 0
+input.onButtonPressed(Button.A, function () {
+    bird.change(LedSpriteProperty.Y, -1)
+})
+input.onButtonPressed(Button.B, function () {
+    bird.change(LedSpriteProperty.Y, 1)
+})
+index = 0
+obstacles = []
+bird = game.createSprite(0, 2)
+bird.set(LedSpriteProperty.Blink, 300)
+game.setScore(0)
+basic.forever(function () {
+    while (obstacles.length > 0 && obstacles[0].get(LedSpriteProperty.X) == 0) {
+        obstacles.removeAt(0).delete()
+    }
+    for (let obstacle2 of obstacles) {
+        obstacle2.change(LedSpriteProperty.X, -1)
+    }
+    if (ticks % 3 == 0) {
+        emptyObstacleY = Math.randomRange(0, 4)
+        for (let index2 = 0; index2 <= 4; index2++) {
+            if (index2 != emptyObstacleY) {
+                obstacles.push(game.createSprite(4, index2))
+            }
+        }
+    }
+    for (let obstacle3 of obstacles) {
+        if (obstacle3.get(LedSpriteProperty.X) == bird.get(LedSpriteProperty.X) && obstacle3.get(LedSpriteProperty.Y) == bird.get(LedSpriteProperty.Y)) {
+            basic.clearScreen()
+            basic.showLeds(`
+                # # # # #
+                # # # # #
+                # # # # #
+                # # # # #
+                # # # # #
+                `)
+            basic.showLeds(`
+                . . . . .
+                . # # # .
+                . # # # .
+                . # # # .
+                . . . . .
+                `)
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . # . .
+                . . . . .
+                . . . . .
+                `)
+            basic.showString("Whoopsy doopsy you died in a swhoop")
+        }
+    }
+    ticks += 1
+    basic.pause(250)
+})
